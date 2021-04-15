ddfb2595dfaaa543c7eec975ecbc34c04e8dadf4
NBKeerthana <70811915+NBKeerthana@users.noreply.github.com>
2021-04-15 20:17:38 +0530

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -3,7 +3,7 @@ class Tree{
         var options={
             isStatic:true
         }
-        //this.image = loadImage("Plucking mangoes/tree.png")
+        this.image = loadImage("Plucking mangoes/tree.png")
         this.body = Bodies.rectangle(x,y,width,height,options)
         this.width = width
         this.height = height
@@ -12,10 +12,10 @@ class Tree{
     display(){
         push()
         translate(this.body.position.x,this.body.position.y)
-        //imageMode(CENTER)
-        //image(this.image,0,0,this.width,this.height)
-        rectMode(CENTER)
-        rect(0,0,this.width,this.height)
+        imageMode(CENTER)
+        image(this.image,0,0,this.width*10,this.height*10)
+        //rectMode(CENTER)
+        //rect(0,0,this.width,this.height)
         pop()
     }
 }
