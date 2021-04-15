bdc0a94b3e087a4efd08917f43f071a0c65e871a
NBKeerthana <70811915+NBKeerthana@users.noreply.github.com>
2021-04-15 20:14:44 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -1,22 +1,21 @@
 class Stone{
-    constructor(x,y,width,height){
+    constructor(x,y,r){
         var options={
             isStatic:false,
             restitution:0,
             friction:1,
             density:1.2
         }
-        this.body = Bodies.rectangle(x,y,width,height,options)
-        this.width = width
-        this.height = height
         this.image = loadImage("Plucking mangoes/stone.png")
+        this.body = Bodies.circle(x,y,r,options)
+        this.r=r
         World.add(world, this.body)
     }
     display(){
         push()
         translate(this.body.position.x,this.body.position.y)
         imageMode(CENTER)
-        image(this.image,0,0,this.width,this.height)
+        image(this.image,0,0,this.r,this.r)
         pop()
     }
 }
